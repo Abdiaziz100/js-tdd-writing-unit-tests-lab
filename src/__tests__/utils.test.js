595a3d03af0a35a3cd5b151b017e318e8c2c1fc9
mohamed <abdiazizmahat@gmail.com>
2025-07-27 22:57:44 +0300

    hghhg

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,17 @@
-// Your tests here
+import { isPalindrome } from '../utils';
+
+test('is case-insensitive', () => {
+  expect(isPalindrome('RaceCar')).toBe(true);
+});
+
+test('returns false for an empty string', () => {
+  expect(isPalindrome('')).toBe(false);
+});
+
+test('throws error for words with non-alphabetic characters', () => {
+  expect(() => isPalindrome('race-car')).toThrow("Only alphabetic characters are allowed");
+});
+
+test('throws error for non-string input', () => {
+  expect(() => isPalindrome(12321)).toThrow("Input must be a string");
+});
